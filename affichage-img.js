978d5f0c079d27b680cfedf2557777b8482e7eae
numeriquesEnsaama <164875294+numeriquesEnsaama@users.noreply.github.com>
2024-03-25 14:04:23 +0100

    Add files via upload

---
+++ affichage-img.js
@@ -0,0 +1,134 @@
+// etienne
+const etienne = document.getElementById('etienne');
+const etienneImg = document.getElementById('etienneImg');
+// mathilde
+const mathilde = document.getElementById('mathilde');
+const mathildeImg = document.getElementById('mathildeImg');
+// flora
+const flora = document.getElementById('flora');
+const floraImg = document.getElementById('floraImg');
+// hanna
+const hanna = document.getElementById('hanna');
+const hannaImg = document.getElementById('hannaImg');
+// iris
+const iris = document.getElementById('iris');
+const irisImg = document.getElementById('irisImg');
+// theo
+const theo = document.getElementById('theo');
+const theoImg = document.getElementById('theoImg');
+// tristan
+const tristan = document.getElementById('tristan');
+const tristanImg = document.getElementById('tristanImg');
+// leo
+const leo = document.getElementById('leo');
+const leoImg = document.getElementById('leoImg');
+// jordan
+const jordan = document.getElementById('jordan');
+const jordanImg = document.getElementById('jordanImg');
+// jennifer
+const jennifer = document.getElementById('jennifer');
+const jenniferImg = document.getElementById('jenniferImg');
+// francois
+const francois = document.getElementById('francois');
+const francoisImg = document.getElementById('francoisImg');
+// helio
+const helio = document.getElementById('helio');
+const helioImg = document.getElementById('helioImg');
+
+// etienne
+etienne.addEventListener('mouseover', () => {
+    etienneImg.style.opacity = 1; 
+    console.log("Bonjour, ceci est un message dans la console !");
+});
+etienne.addEventListener('mouseout', () => {
+    etienneImg.style.opacity = 0;
+});
+
+// mathilde
+mathilde.addEventListener('mouseover', () => {
+    mathildeImg.style.opacity = 1; 
+    console.log("Bonjour, ceci est un message dans la console !");
+});
+mathilde.addEventListener('mouseout', () => {
+    mathildeImg.style.opacity = 0;
+});
+
+// flora
+flora.addEventListener('mouseover', () => {
+    floraImg.style.opacity = 1; 
+});
+flora.addEventListener('mouseout', () => {
+    floraImg.style.opacity = 0;
+});
+
+// hanna
+hanna.addEventListener('mouseover', () => {
+    hannaImg.style.opacity = 1; 
+});
+hanna.addEventListener('mouseout', () => {
+    hannaImg.style.opacity = 0;
+});
+
+// iris
+iris.addEventListener('mouseover', () => {
+    irisImg.style.opacity = 1; 
+});
+iris.addEventListener('mouseout', () => {
+    irisImg.style.opacity = 0;
+});
+
+// theo
+theo.addEventListener('mouseover', () => {
+    theoImg.style.opacity = 1; 
+});
+theo.addEventListener('mouseout', () => {
+    theoImg.style.opacity = 0;
+});
+
+// tristan
+tristan.addEventListener('mouseover', () => {
+    tristanImg.style.opacity = 1; 
+});
+tristan.addEventListener('mouseout', () => {
+    tristanImg.style.opacity = 0;
+});
+
+// leo
+leo.addEventListener('mouseover', () => {
+    leoImg.style.opacity = 1; 
+});
+leo.addEventListener('mouseout', () => {
+    leoImg.style.opacity = 0;
+});
+
+// jordan
+jordan.addEventListener('mouseover', () => {
+    jordanImg.style.opacity = 1; 
+});
+jordan.addEventListener('mouseout', () => {
+    jordanImg.style.opacity = 0;
+});
+
+// jennifer
+jennifer.addEventListener('mouseover', () => {
+    jenniferImg.style.opacity = 1; 
+});
+jennifer.addEventListener('mouseout', () => {
+    jenniferImg.style.opacity = 0;
+});
+
+// francois
+francois.addEventListener('mouseover', () => {
+    francoisImg.style.opacity = 1; 
+});
+francois.addEventListener('mouseout', () => {
+    francoisImg.style.opacity = 0;
+});
+
+// helio
+helio.addEventListener('mouseover', () => {
+    helioImg.style.opacity = 1; 
+});
+helio.addEventListener('mouseout', () => {
+    helioImg.style.opacity = 0;
+});
---
+++ curseur.js
@@ -0,0 +1,6 @@
+// curseur
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) =>{
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
